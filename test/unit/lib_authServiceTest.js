5394d78843add815f9c27289a2a76a935b491941
Marcel Wagner <wagmarcel@web.de>
2021-02-13 15:54:06 +0100

    improved authservice tests

--- test/unit/lib_authServiceTest.js
+++ test/unit/lib_authServiceTest.js
@@ -25,13 +25,50 @@ var fileToTest = "../../lib/authService/authenticate.js";
 describe(fileToTest, function(){
     var toTest = rewire(fileToTest);
 
-    it('Shall send successful request to validate token', function(done){
+    it('Shall verify and decode token successfully', function(done){
+        var decodedToken = {sub: "1234"};
+        var verifier = function(token) {
+            return decodedToken;
+        }
+        toTest.__set__("verifier",verifier);
+        var me = {
+            logger: {
+                info: function() {},
+                debug: function() {}
+            }
+        }
+        toTest.__set__("me", me);
+        var verifyAndDecodeToken = toTest.__get__("verifyAndDecodeToken");
+        var result = verifyAndDecodeToken("ex1123")
+        assert.equal(decodedToken, result, "Wrong decoded Token");
+        done();
+    });
+    it('Shall verify and decode token unsuccessfully', function(done){
+        var decodedToken = {sub: "1234"};
+        var message = "No valid token";
+        var verifier = function(token) {
+            throw new Error(message);
+        }
+        toTest.__set__("verifier",verifier);
+        var me = {
+            logger: {
+                info: function() {},
+                debug: function() {}
+            }
+        }
+        toTest.__set__("me", me);
+        var verifyAndDecodeToken = toTest.__get__("verifyAndDecodeToken");
+        var result = verifyAndDecodeToken("ex1123");
+        assert.equal(result, null, "Wrong verfication result");
+        done();
+    })
+    it('Shall receive public key', function(done){
         var request = function(options, callback) {
             assert.equal(options.method, "GET", "wrong request method");
-            assert.equal(options.url, "http://keycloakurl:1234/auth/realms/realm/protocol/openid-connect/userinfo", "wrong keycloak url");
-            assert.equal(options.headers.Authorization, "Bearer token", "wrong Authorization header");
+            assert.equal(options.url, "http://keycloakurl:1234/auth/realms/realm", "wrong keycloak url");
             var response = {
-                statusCode: 200
+                statusCode: 200,
+                body: "{\"public_key\": \"publicKey\"}"
             }
             callback(null, response);
         }
@@ -42,67 +79,129 @@ describe(fileToTest, function(){
             }
         }
         toTest.__set__("me", me);
-        var validateToken = toTest.__get__("validateToken");
-        validateToken("http://keycloakurl:1234/auth", "realm", "token")
+        var getPublicKeyForRealm = toTest.__get__("getPublicKeyForRealm");
+        getPublicKeyForRealm("http://keycloakurl:1234/auth", "realm")
         .then((result) => {
-            if (result) {
-                done();
-            } else {
-                done("Wrong validation result");
-            }
-        });
-    })
-    it('Shall send unsuccessful request to validate token', function(done){
-        var request = function(options, callback) {
-            assert.equal(options.method, "GET", "wrong request method");
-            assert.equal(options.url, "http://keycloakurl:1234/auth/realms/realm/protocol/openid-connect/userinfo", "wrong keycloak url");
-            assert.equal(options.headers.Authorization, "Bearer token", "wrong Authorization header");
-            var response = {
-                statusCode: 400
-            }
-            callback(null, response);
-        }
-        toTest.__set__("request",request);
-        var me = {
-            logger: {
-                debug: function() {}
-            }
-        }
-        toTest.__set__("me", me);
-        var validateToken = toTest.__get__("validateToken");
-        validateToken("http://keycloakurl:1234/auth", "realm", "token")
-        .then((result) => {
-            if (!result) {
-                done();
-            } else {
-                done("Wrong validation result");
-            }
-        });
-    })
-    it('Shall throw error while validating token', function(done){
-        var request = function(options, callback) {
-            assert.equal(options.method, "GET", "wrong request method");
-            assert.equal(options.url, "http://keycloakurl:1234/auth/realms/realm/protocol/openid-connect/userinfo", "wrong keycloak url");
-            assert.equal(options.headers.Authorization, "Bearer token", "wrong Authorization header");
-            var response = {
-                statusCode: 400
-            }
-            callback("Error", null);
-        }
-        toTest.__set__("request",request);
-        var me = {
-            logger: {
-                debug: function() {}
-            }
-        }
-        toTest.__set__("me", me);
-        var validateToken = toTest.__get__("validateToken");
-        validateToken("http://keycloakurl:1234/auth", "realm", "token")
+            assert.equal("-----BEGIN PUBLIC KEY-----\npublicKey\n-----END PUBLIC KEY-----", result, "Incorrect public key received");
+            done();
+        })
         .catch((e) => {
-            assert.equal(e.message, "Error", "Wrong error thrown");
-            done()
+            done("Unexpected exception: " + e.message)
         });
-    })
+    });
+    it('Shall authenticate super user', function(done){
+        var config = {
+            broker: {
+                username: "username",
+                password: "password"
+            }   
+        }
+        var logger = {
+            debug: function() {},
+            info: function() {}
+        }
+        var req = {
+            query: {
+                username: "username",
+                password: "password" 
+            }
+        }
+        var res = {
+            sendStatus: function(status) {
+                assert.equal(status, 200, "Received wrong status");
+                done();
+            }
+        }
+        /*var me = {
+            logger: logger,
+            public_key: "publicKey"
+        }*/
+        var authenticate = new toTest(config, logger);
+        authenticate.authenticate(req, res)
+    });
+    it('Authentication shall complain about missing public key ', function(done){
+        var config = {
+            broker: {
+                username: "username",
+                password: "password"
+            }   
+        }
+        var logger = {
+            debug: function() {},
+            info: function() {}
+        }
+        var req = {
+            query: {
+                username: "username2",
+                password: "password2" 
+            }
+        }
+        var res = {
+            sendStatus: function(status) {
+                assert.equal(status, 400, "Received wrong status");
+                done();
+            }
+        }
+        /*var me = {
+            logger: logger,
+            public_key: "publicKey"
+        }*/
+        var authenticate = new toTest(config, logger);
+        authenticate.authenticate(req, res)
+    });
+    it('Authentication shall successfully validate a token', function(done){
+        var decodedToken = {
+            sub: "deviceId", 
+            accounts: [{
+                role: "device",
+                id: "accountId"
+            }]
+        };
+        var verifier = function(token) {
+            return decodedToken;
+        }
+        toTest.__set__("verifier",verifier);
+        var config = {
+            broker: {
+                username: "username",
+                password: "password"
+            }   
+        }
+        var logger = {
+            debug: function() {},
+            info: function() {}
+        }
+        var req = {
+            query: {
+                username: "deviceId",
+                password: "token" 
+            }
+        }
+        var res = {
+            sendStatus: function(status) {
+                assert.equal(status, 200, "Received wrong status");
+                done();
+            }
+        }
+        var cache = {
+            setValue: function(key, type, value) {
+                assert.equal(key, "accountId/deviceId", "Wrong cache value received.");
+                assert.equal(type, "acl", "Wrong cache value received.");
+                assert.equal(value, true, "Wrong cache value received.");
+            }
+        }
+        var me = {
+            logger: logger,
+            config: config,
+            public_key: "publicKey",
+            cache: cache
+        }
+        var authenticate = new toTest(config, logger);
+        var me = toTest.__get__("me", me);
+        me.public_key = "publicKey";
+        me.cache = cache;
+        authenticate.authenticate(req, res)
+    });
     /*it('Shall check url parameters to check username/password', function(done){
         var config = {};
         var logger = {
